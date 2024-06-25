f24ca435f71ffafc6b4ddab7d48b25f4db923078
flaku <falk@draftcode.de>
2024-06-25 10:49:50 +0200

    working with statements

---
+++ logical_operators.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+has_good_credit = True
+has_criminal_record = False
+
+if (has_good_credit and not(has_criminal_record)):
+    print(f"Yout get Loan.")
+else:
+    print(f"Nope not for you.")
